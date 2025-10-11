b57f3e4e9de43cb8de95190a044435fc6a818a6c
Poojitha Eragoni <poojithaeragoni@gmail.com>
2025-10-11 20:39:53 +0530

    Aggressive_cows.java

--- Aggressive_cows.java
+++ Aggressive_cows.java
@@ -1,3 +1,6 @@
+//given an array with unique elements of stalls[], which denote the positions of stalls. 
+//And also given an integer k which denotes the number of aggressive cows.
+//The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible
 import java.util.Arrays;
 
 public class Aggressive_cows {
