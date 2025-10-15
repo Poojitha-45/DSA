0346c39d555bd3c3868908d2f10350144728f9f1
Poojitha Eragoni <poojithaeragoni@gmail.com>
2025-10-15 19:39:41 +0530

    Coin_Change.java

--- Coin_Change.java
+++ Coin_Change.java
@@ -1,7 +1,10 @@
+//Given an integer array coins[ ] representing different denominations of currency and an integer sum,
+//find the number of ways you can make sum by using different combinations from coins[ ]. 
+//Assume that you have an infinite supply of each type of coin. Therefore, you can use any coin as many times as you want.
 public class Coin_Change {
     public int count(int coins[],  int sum) {
       int  n = coins.length;
-        long[] dp = new long[sum + 1];
+        long[] dp = new long[sum + 1];  // solved using dynamic programming
         dp[0] = 1;
 
         for (int i = 0; i < n; i++) {
@@ -18,6 +21,6 @@ public class Coin_Change {
         int[] coins = {1, 2, 3};
         int sum = 4;
         int n = coins.length;
-        System.out.println("Number of ways to make " + sum + " is: " + sol.count(coins, sum));
+        System.out.println("Number of ways to make " + sum + " is: " + sol.count(coins, sum));// output : 4
     }
 }
