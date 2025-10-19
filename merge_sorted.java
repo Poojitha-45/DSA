9c4c3d36cb1ca6e4fa9b499f7a7e4881737560c2
Poojitha Eragoni <poojithaeragoni@gmail.com>
2025-10-19 18:24:32 +0530

    merge_sorted.java
    Add comments explaining the merge function's purpose and parameters.

--- merge_sorted.java
+++ merge_sorted.java
@@ -1,3 +1,6 @@
+// given Two integer arrays nums1 and nums2, sorted in non-decreasing order,
+//and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 public class merge_sorted {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
        int i = m-1;
