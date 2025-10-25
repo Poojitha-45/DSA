68fe7db1900d70e5c48fc121b9ec571327c973cf
Poojitha Eragoni <poojithaeragoni@gmail.com>
2025-10-25 13:35:25 +0530

    next_smaller(Stack)
    Added output comment to clarify the result of the next smaller element calculation.

--- next_smaller.java
+++ next_smaller.java
@@ -1,3 +1,6 @@
+//Given an integer array arr[ ]. For every element in the array, the task is to determine its Next Smaller Element (NSE).
+//The Next Smaller Element (NSE) of an element x is the first element that appears to the right of x in the array and is strictly smaller than x.
+//If no such element exists, assign -1 as the NSE for that position.
 import java.util.*;
 
 public class next_smaller {
@@ -19,6 +22,6 @@ public class next_smaller {
     public static void main(String args[]) {
         int arr[] = {4, 8, 5, 2, 25};
         int ans[] = nextsmallelement(arr);
-        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(ans));// output:[2, 5, 2, -1, -1]
     }
 }
