7d3e777b88bd6b7be2a79aac94b437d8011b054b
Your Name <poojithaeragoni@gmail.com>
2025-10-29 07:39:26 +0530

    space

---
+++ backspace_compare.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class backspace_compare {
+    public static boolean backspaceCompare(String s, String t){
+    Stack<Character> st1 = new Stack<>();
+    Stack<Character> st2 = new Stack<>();
+    for(int i=0;i<s.length();i++){
+        char c = s.charAt(i);
+        if(c == '#'){
+            if(!st1.isEmpty()){
+                st1.pop();
+            }
+        }else{
+                st1.push(c);
+            }
+    }
+    for(int i=0;i<t.length();i++){
+        char k = t.charAt(i);
+        if(k == '#'){
+            if(!st2.isEmpty()){
+                st2.pop();
+            }
+        }else{
+                st2.push(k);
+            }
+        }
+    return st1.equals(st2);
+    }
+    public static void main(String args[]){
+        String s = "ab#c";
+        String t = "ad#c";
+        System.out.println(backspace_compare.backspaceCompare(s,t));
+    }
+}
