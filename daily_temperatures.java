994c3f899c980609eec57c5dc415f44667edbe3f
Poojitha Eragoni <poojithaeragoni@gmail.com>
2025-10-31 07:11:48 +0530

    next high temperatures(Stack)
    Updated comments and formatting in daily_temperatures.java.

--- daily_temperatures.java
+++ daily_temperatures.java
@@ -1,3 +1,6 @@
+//Given an array of integers temperatures represents the daily temperatures, 
+//return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
+//If there is no future day for which this is possible, keep answer[i] == 0 instead.
 import java.util.*;
 public class daily_temperatures {
     public static int[] nextHighTemp(int arr[]){
@@ -17,6 +20,6 @@ public class daily_temperatures {
     }
     public static void main(String args[]){
         int arr[] = {73,74,75,71,69,72,76,73};
-        System.out.println(" next high temperatures are : " + Arrays.toString(nextHighTemp(arr)));
+        System.out.println(" next high temperatures are : " + Arrays.toString(nextHighTemp(arr)));//output:next high temperatures are : [1, 1, 4, 2, 1, 1, 0, 0]
     }
 }
